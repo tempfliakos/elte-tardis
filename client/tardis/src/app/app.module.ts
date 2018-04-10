fd526c2f88e175d114e4284a9822673cd8730c25
tema <tempfliakos@inf.elte.hu>
2018-04-10 23:40:44 +0200

    Revert "Tárgy hozzáadás létrehozása, alap funkciók definiálása"
    This reverts commit 8dd09347be31581143790aba0ee2b0457b6efcdc.

--- client/tardis/src/app/app.module.ts
+++ client/tardis/src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { AddingSubjectsComponent } from './components/adding-subjects/adding-subjects.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AddingSubjectsComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    HttpModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
